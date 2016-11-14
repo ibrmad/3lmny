8ddc19e2ce08d874015dc9be02b6c8644741fa6d
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2016-11-14 22:04:01 +0200

    add db role to the production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '138.68.134.128', user: 'deploy', roles: %w{web app}
+server '138.68.134.128', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
