ce4731b1b802cfd89cdb240b2f30071b1eb7ac86
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-01-04 15:38:27 +0200

    asd

--- db/migrate/20161220190257_create_comments.rb
+++ db/migrate/20161220190257_create_comments.rb
@@ -1,6 +1,15 @@
 class CreateComments < ActiveRecord::Migration[5.0]
-  def change
+  def up
     create_table :comments do |t|
+      t.text :content
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamps
+    end
+  end
+  def down
+    drop_table :comments do |t|
       t.text :content
       t.integer :user_id
       t.integer :post_id
