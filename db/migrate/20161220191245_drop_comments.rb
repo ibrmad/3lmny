fb78159bcd4748c79e46c8698bc1f2d95bb7dfa2
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-01-04 15:16:05 +0200

    added the admin panel

---
+++ db/migrate/20161220191245_drop_comments.rb
@@ -0,0 +1,11 @@
+class DropComments < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :comments do |t|
+      t.text :content
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamps
+    end
+  end
+end
