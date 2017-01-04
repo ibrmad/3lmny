daab5f84c1dada1d7d4b3d016f3d85f002038e53
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-01-04 15:31:48 +0200

    added the admin panel

--- db/migrate/20161220190257_create_comments.rb
+++ db/migrate/20161220190257_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
-    create_table :comments do |t|
+    drop_table :comments do |t|
       t.text :content
       t.integer :user_id
       t.integer :post_id
