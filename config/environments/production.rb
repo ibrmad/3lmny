7b858d3a3cb3ec0612a4f7418128d5da35a3ea1d
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-01-07 22:10:25 +0200

    fixed things up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'ibrahim.alh94@gmail.com',
+      password:             'G6ibra?g7;',
+      authentication:       :plain,
+      enable_starttls_auto: true
+    }
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
