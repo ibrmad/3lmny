1adba097b5905555ca04772eae7b7e315eda4e90
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-01-07 22:14:06 +0200

    fixed things up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => '138.68.134.128' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
