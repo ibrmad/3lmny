74341a0f305004146d09bd51d0e022277c1a5065
Ibrahim Alhamad <ibrahim.alh94@gmail.com>
2017-11-07 09:29:58 +0200

    fixing issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'ibrahim.alh94@gmail.com',
-    password:             'G6ibra?g7;',
+    user_name:            '',
+    password:             '',
     authentication:       :plain,
     enable_starttls_auto: true
   }
